d51ef68fd12baeb9e78313adb22d2fc40ff59088
Lucas <lucasmenezes3333@gmail.com>
2024-10-14 14:17:10 -0300

    Fiz alterações no menu (coloquei menu de opções) e alteração na main

--- src/view/menu.py
+++ src/view/menu.py
@@ -1,4 +1,15 @@
-def opcoes ():
-    print ("1- treino")
+def menu_opcoes ():
+    print ("Menu de opções\n")
+    print ("1- Adicionar registro de treino/competição")
+    print ("2- Visualizar registro de treino/competição")
+    print ("3- Atualizar registro de treino/competição")
+    print ("4- Excluir registro de treino/competição")
     return
-    
+
+def opcoes ():
+    opcao = input ("Digite o código da ação correspondente: ")
+    if (opcao == 1):
+        from controllers.treino_controller import adicionar_treino
+    elif (opcao == 2):
+        from controllers.treino_controller import listar_treinos
+    return
