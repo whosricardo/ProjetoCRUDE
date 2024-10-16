1b94181dc59eb6baa71398d5090427f89b80d3fc
Lucas <lucasmenezes3333@gmail.com>
2024-10-16 09:47:27 -0300

    Interfaçe Menu/Interação

--- src/view/menu.py
+++ src/view/menu.py
@@ -4,12 +4,25 @@ def menu_opcoes ():
     print ("2- Visualizar registro de treino/competição")
     print ("3- Atualizar registro de treino/competição")
     print ("4- Excluir registro de treino/competição")
+    print ("5- sair")
     return
 
 def opcoes ():
     opcao = input ("Digite o código da ação correspondente: ")
     if (opcao == 1):
         from controllers.treino_controller import adicionar_treino
-    elif (opcao == 2):
-        from controllers.treino_controller import listar_treinos
-    return
+        nome = input ("Insira o seu nome: ")
+        data = int(input("Insira a data do treino/competição: ""{:%d/%m/%Y}".format(data)))
+        tempo = input("Insira o tempo de treino/competição em minutos: ")
+        distancia = float(input("Insira a distancia percorrida em quilometros: "))
+        clima = input("Insira o clima do treino/competição correspondente : ")
+        localizacao = input("Insira a localização do treino/competição: ")
+        adicionar_treino (nome,data,tempo,distancia,clima,localizacao)
+        
+        
+        
+        
+        
+        
+        
+  
