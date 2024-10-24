6cf508bae8f44b1a72af5561165e6342bcd564d9
Lucas <lucasmenezes3333@gmail.com>
2024-10-24 09:21:01 -0300

    Adicionei a opcao de visualizar treinos e a de adicionar no menu

--- src/view/menu.py
+++ src/view/menu.py
@@ -9,6 +9,24 @@ def menu_opcoes ():
 def opcoes ():
     opcao = input ("Digite o código da ação correspondente: ")
     if (opcao == 1):
+        opcao_adicionar()
+        
+    elif (opcao == 2):
+        opcao_visualizar()
+        
+    elif (opcao == 3):
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        """
         from controllers.treino_controller import adicionar_treino
         nome = input ("Insira o seu nome: ")
         data = int(input("Insira a data do treino/competição: ""{:%d/%m/%Y}".format(data)))
@@ -17,8 +35,10 @@ def opcoes ():
         clima = input("Insira o clima do treino/competição correspondente : ")
         localizacao = input("Insira a localização do treino/competição: ")
         adicionar_treino (nome,data,tempo,distancia,clima,localizacao)
+        """
 
-
+def opcao_adicionar ():
+    from controllers.treino_controller import adicionar_treino
     while True:
         try:
             nome = str(input("Insira o seu nome: "))
@@ -30,12 +50,17 @@ def opcoes ():
             break
         except ValueError:
             print("Insira por favor um valor correspondente ao que foi pedido")
-
+    
+    adicionar_treino (nome,data,tempo,distancia,clima,localizacao)
+    
+    
+def opcao_visualizar ():
+    from controllers.treino_controller import listar_treinos
+    listar_treinos()
+    
         
-        
-        
-        
-        
+def opcao_atualizar():
+          
         
         
   
